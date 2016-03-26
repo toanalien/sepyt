4c9904b3b0d2b77bcadc39eafcaa2c3d1017e617
Toan Vo <toanalien@gmail.com>
2016-03-26 11:16:27 +0700

    add index view

--- NewToDjango/polls/urls.py
+++ NewToDjango/polls/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'), # render view.index for root path
+    url(r'^$', views.index, name='index'), # /polls/
 ]
--- NewToDjango/polls/views.py
+++ NewToDjango/polls/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # get 5 latest questions
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # render polls/index.html
+    return HttpResponse(template.render(context, request))
