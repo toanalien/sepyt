75269664e369c2271e15ecb8dc9981e52544b159
Toan Vo <toanalien@gmail.com>
2016-03-26 11:38:07 +0700

    add vote view, and detail view

--- NewToDjango/polls/urls.py
+++ NewToDjango/polls/urls.py
@@ -7,4 +7,6 @@ app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'), # /polls/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 ]
